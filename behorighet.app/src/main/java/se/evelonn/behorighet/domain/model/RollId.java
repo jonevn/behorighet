87c3babaa594cb7f91e9ca3644d20ced2451f183
Johan Evelönn <johanevelonn@gmail.com>
2016-02-19 22:47:14 +0100

    ändrade till publik konstruktor, adderade equals och hashcode

--- behorighet.app/src/main/java/se/evelonn/behorighet/domain/model/RollId.java
+++ behorighet.app/src/main/java/se/evelonn/behorighet/domain/model/RollId.java
@@ -12,7 +12,7 @@ public class RollId implements Serializable {
 	@Column(name = "id", columnDefinition = "BINARY(16)")
 	private UUID id;
 
-	protected RollId() {
+	public RollId() {
 		// JPA
 	}
 
@@ -35,4 +35,35 @@ public class RollId implements Serializable {
 	public String värde() {
 		return id.toString();
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		RollId other = (RollId) obj;
+		if (id == null) {
+			if (other.id != null) {
+				return false;
+			}
+		} else if (!id.equals(other.id)) {
+			return false;
+		}
+		return true;
+	}
+
 }
